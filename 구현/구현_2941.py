d37f0f9edc2ec1a7c44a14519936a26a76dd3488
Jaewon Lee <57023560+leejw7650@users.noreply.github.com>
2022-09-27 18:52:42 +0900

    Delete 구현 directory

--- 구현/구현_2941.py
+++
@@ -1,9 +0,0 @@
-n = input()
-
-cro = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
-
-for i in cro :
-    if i in n :
-        n = n.replace(i, 'a')
-
-print(len(n))
--- 구현/구현_Q7.py
+++
@@ -1,9 +0,0 @@
-score = input()
-
-score = list(map(int,score))
-
-if sum(score[0:(len(score)//2)]) == sum(score[-(len(score)//2):]) :
-    print("LUCKY")
-
-else :
-    print("READY")
