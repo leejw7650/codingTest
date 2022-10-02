38784f5785e447c6f00857ec64c28131d05cacb8
Jaewon Lee <57023560+leejw7650@users.noreply.github.com>
2022-10-02 12:01:14 +0900

    Add files via upload

---
+++ DFS_BFS/DFS_BFS_1697.py
@@ -0,0 +1,41 @@
+# 문제
+# 수빈이는 동생과 숨바꼭질을 하고 있다. 수빈이는 현재 점 N(0 ≤ N ≤ 100,000)에 있고, 동생은 점 K(0 ≤ K ≤ 100,000)에 있다. 수빈이는 걷거나 순간이동을 할 수 있다. 만약, 수빈이의 위치가 X일 때 걷는다면 1초 후에 X-1 또는 X+1로 이동하게 된다. 순간이동을 하는 경우에는 1초 후에 2*X의 위치로 이동하게 된다.
+
+# 수빈이와 동생의 위치가 주어졌을 때, 수빈이가 동생을 찾을 수 있는 가장 빠른 시간이 몇 초 후인지 구하는 프로그램을 작성하시오.
+
+# 입력
+# 첫 번째 줄에 수빈이가 있는 위치 N과 동생이 있는 위치 K가 주어진다. N과 K는 정수이다.
+
+# 출력
+# 수빈이가 동생을 찾는 가장 빠른 시간을 출력한다.
+
+from collections import deque
+n, k = map(int, input().split())
+
+visited = [0] * 100001
+time = [0] * 100001
+
+def sol(n,k) :
+    queue = deque()
+
+    if n > k :
+        return n - k
+    elif n == k :
+        return 0
+        
+    queue.append(n)
+    while time[k] == 0 :
+        
+        n = queue.popleft()
+        visited[n] = 1
+        for i in [n-1, n+1, 2*n] :
+            if i < 0 or i > 100000 or i > 100000 :
+                continue
+            if visited[i] == 0:
+                queue.append(i)
+                visited[i] = 1
+                time[i] = time[n] + 1
+
+    return time[k]
+
+print(sol(n,k))
